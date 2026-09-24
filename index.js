c8d2e446dca04712ca210422f7df96f5263219e0
JuliaSilvaXP <jusilva260309@gmail.com>
2026-09-24 15:50:14 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function mostrarTela(nome){
-            const telas = document.querySelectorAll('section')
-            telas.forEach(telas => {
-                telas.style.display = 'none';
-            });
-
-            const telaEscolhida = document.getElementById('tela-'+ nome);
-            telaEscolhida.style.display = "block";
-        }
